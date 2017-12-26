a1c0a4ba9214441f741cfad37ab124d0084c7f34
h3xt3k <jojoewig@web.de>
2017-12-26 13:59:27 +0100

    merge

--- app.js
+++ app.js
@@ -2,6 +2,11 @@ const express = require('express')
 const app = express()
 const fs = require('fs');
 const path = require('path');
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
+const dburl = 'mongodb://localhost:27017/little-helper';
+const dbName = 'little-helper';
+
 var bodyParser = require('body-parser')
     app.use( bodyParser.json() );       // to support JSON-encoded bodies
     app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
@@ -9,13 +14,12 @@ var bodyParser = require('body-parser')
 }));
 
 app.get('/', (req, res) => res.send('Hello World!'))
-app.post('/copyNewest', function (req, res) {
-    console.log('copyNewest')
+app.post('/copyNewest', (req, res) => {
     copyFile();
     res.send('Got a POST request')
     //res.download
 })
-app.post('/copyTimeroom', function (req, res) {
+app.post('/copyTimeroom', (req, res) => {
     console.log('copyTimeroom');
     console.log(req);
 })
@@ -23,39 +27,72 @@ app.use(express.static('public'))
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
 
+// MongoClient.connect(dburl, function(err, client) {
+//     assert.equal(null, err);
+//     console.log("Connected successfully to server");
+//
+//     const db = client.db(dbName);
+//     db.createCollection("little-helper", function(err, res) {
+//         if (err) throw err;
+//         console.log("Collection created!");
+//         // db.close();
+//     });
+//
+//     client.close();
+// });
+
+
 function log_string(string){
     console.log(string)
 }
 
 function copyFile() {
-    var fs = require('fs');
+    var fileslist = [];
+    var path = '/users/jojoe/desktop';
+    // var path = '/root/little-helper/from/test.jpg';
+    var path = '/home/pi/photos'
 
-    // destination.txt will be created or overwritten by default.
-    fs.copyFile('/root/little-helper/from/test.jpg', '/root/destination.jpg', (err) => {
-        if (err) throw err;
-        console.log('source.jpg was copied to destination.jpg');
+    MongoClient.connect(dburl, function(err, client) {
+        assert.equal(null, err);
+        console.log("Connected successfully to server");
+        const lhdatabase = client.db("little-helper");
+        var collection = lhdatabase.collection("little-helper");
+
+        fs.readdir(path, (err, files) => {
+            var nextindex = 0;
+            collection.find({}).toArray((err, docs) => {
+                docs.forEach(doc => {
+                    if(nextindex < doc._id){
+                        nextindex = doc._id + 1;
+                    }
+                    files.forEach(file => {
+                        console.log(nextindex);
+                        var obj = {}
+                        obj.filename = file;
+                        obj._id = nextindex;
+                        fileslist.push(obj)
+                        nextindex += 1;
+                    });
+                });
+                console.log(fileslist);
+                collection.insertMany(fileslist, (err, result) => {
+                    // console.log(result);
+                    if (err) {
+                        console.log(err);
+                    }
+                    console.log("successfully");
+                });
+            });
+        });
     });
+
     //
-    // let src = path.join(__dirname, 'from/test.txt');
-    // let destDir = path.join(__dirname, 'to');
-    // let readStream = fs.createReadStream(src);
-    // let filename = 'test.txt';
-    // let dest = path.join(destDir, filename)
-    // fs.access(destDir, (err) => {
-    //     if(err)
-    //         fs.mkdirSync(destDir);
-    //
-    //         copyFile(src, path.join(destDir, filename));
+    // fs.stat(path, function(err, stats) {
+    //     console.log(stats.mtime);
+    // })
+
+    // fs.copyFile(path, '/root/destination.jpg', (err) => {
+    //     if (err) throw err;
+    //     console.log('source.jpg was copied to destination.jpg');
     // });
-    //
-    //
-    // readStream.once('error', (err) => {
-    //     console.log(err);
-    // });
-    //
-    // readStream.once('end', () => {
-    //     console.log('done copying');
-    // });
-    //
-    // readStream.pipe(fs.createWriteStream(dest));
 }
