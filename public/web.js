17a3cffb5e687dc9a5203b33c4498df0ddf00700
tincher <joel.ewig@gmail.com>
2017-12-30 17:54:04 +0100

    urls changed

--- public/web.js
+++ public/web.js
@@ -1,11 +1,12 @@
-$(document).ready(function () {
+$(document).ready(function() {
 
     $("#button-newest").click(function(e) {
         e.preventDefault();
         $.ajax({
             type: "POST",
-            url: "http://localhost:3000/copyNewest",
+            // url: "http://localhost:3000/copyNewest",
             // url: "http://85.214.100.20:3000/copyNewest",
+            url: "http://192.168.2.124:3000/copyNewest",
             data: {
                 id: $(this).val(), // < note use of 'this' here
                 access_token: $("#access_token").val()
@@ -22,8 +23,9 @@ $(document).ready(function () {
         e.preventDefault();
         $.ajax({
             type: "POST",
-            url: "http://localhost:3000/copyTimeroom",
+            // url: "http://localhost:3000/copyTimeroom",
             // url: "http://85.214.100.20:3000/copyTimeroom",
+            url: "http://192.168.2.124:3000/copyNewest",
             data: {
                 id: $(this).val(), // < note use of 'this' here
                 access_token: $("#access_token").val()
