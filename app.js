54d3f26a39b76a982d84496d54164dbb28386681
tincher <joel.ewig@gmail.com>
2018-01-04 15:04:25 +0100

    paths updated

--- app.js
+++ app.js
@@ -84,7 +84,11 @@ function log_string(string) {
 
 // copyfile with couchdb
 function copyNewestFiles() {
-    var srcpath = '/users/jojoe/desktop/';
+    // var srcpath = '/users/jojoe/desktop/';
+    var srcpath = '~/android/Interner Gemeinsamer Speicher/DCIM/Camera';
+    var destpath = '~/photos'
+
+
     var dbfiles = [];
     var filenames = [];
     var fileobjects = [];
@@ -122,95 +126,19 @@ function copyNewestFiles() {
                 });
             }
 
-
-
-            // //copy given file (path)
-            // docnames.forEach(docname => {
-            //     progress = 0;
-            //     var srcfile = srcpath + "/" + docname;
-            //     var destfile = destpath + "/" + docname;
-            //     progress = progress + docnames.length / 100;
-            //     console.log("src: " + srcfile);
-            //     console.log("dest: " + destfile);
-            //     fs.copyFile(srcfile, destfile, (err) => {
-            //         if (err) throw err;
-            //         console.log("copied file successfully");
-            //     });
-            // })
+            //copy given file (path)
+            docnames.forEach(docname => {
+                progress = 0;
+                var srcfile = srcpath + "/" + docname;
+                var destfile = destpath + "/" + docname;
+                progress = progress + docnames.length / 100;
+                console.log("src: " + srcfile);
+                console.log("dest: " + destfile);
+                fs.copyFile(srcfile, destfile, (err) => {
+                    if (err) throw err;
+                    console.log("copied file successfully");
+                });
+            })
         });
     });
 }
-
-// // whole copy file thingy TODO needs refactoring
-// // mongodb
-// function copyFile() {
-//     // array of objects for DB
-//     var fileslist = [];
-//     // array of the filenames as strings
-//     var docnames = [];
-//
-//     // config for pi
-//     var destpath = '/home/pi/photos'
-//
-//     // // config for windows
-//     // var srcpath = '/users/jojoe/Desktop'
-//     // var destpath = '/users/jojoe/Desktop/nfolder';
-//
-//     // connect to Mongodb
-//     MongoClient.connect(dburl, function(err, client) {
-//         if (err) {
-//             console.log(err);
-//         }
-//         console.log("Connected successfully to server");
-//
-//         // connect to database and collection
-//         const lhdatabase = client.db("little-helper");
-//         var collection = lhdatabase.collection("little-helper");
-//
-//         // get content of the source directory
-//         fs.readdir(srcpath, (err, files) => {
-//             var nextindex = 0;
-//
-//             // get collection content as array
-//             collection.find().toArray((err, docs) => {
-//
-//                 // var docnames = [];
-//
-//                 // get next index manually TODO automated method
-//                 if (docs.length != 0) {
-//                     docs.forEach(doc => {
-//                         if (nextindex < doc._id) {
-//                             nextindex = doc._id + 1;
-//                         }
-//                         docnames.push(doc.filename);
-//                     });
-//                 }
-//
-//                 // create database-ready objects and push them into an
-//                 // array that will be inserted into the DB
-//                 files.forEach(file => {
-//                     var obj = {}
-//                     obj.filename = file;
-//                     obj._id = nextindex;
-//                     nextindex += 1;
-//                     if (!docnames.includes(file)) {
-//                         fileslist.push(obj);
-//                     }
-//
-//                 });
-//
-//                 //insert the objects into the DB
-//                 if (fileslist.length != 0) {
-//                     collection.insertMany(fileslist, (err, result) => {
-//                         if (err) {
-//                             console.log(err);
-//                         } else {
-//                             console.log("successfully");
-//                         }
-//                     });
-//                 }
-//
-//
-//             });
-//         });
-//     });
